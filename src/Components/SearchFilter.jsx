91a7b769c697a74b8e5a1ca84b790a6c25e65deb
Qaisar <syedqaisartanvir@gmail.com>
2022-10-09 13:11:12 +0500

    Add some Links and input search into searchfilter Component

---
+++ src/Components/SearchFilter.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function SearchFilter() {
+  return (
+    <div className="searchContainer">
+      <div className="searchContainer__input">
+        <input
+          type="text"
+          placeholder="Search Your Desired Product Here ..."
+        ></input>
+      </div>
+    </div>
+  );
+}
+
+export default SearchFilter;

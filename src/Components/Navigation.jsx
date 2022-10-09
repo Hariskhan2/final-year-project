c95ae9525ca9d392ff3b654999d2aab39dcea577
Qaisar <syedqaisartanvir@gmail.com>
2022-10-09 12:34:58 +0500

    Add the route of navigation in the root component

--- src/Components/Navigation.jsx
+++ src/Components/Navigation.jsx
@@ -1,19 +1,19 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
+import "../../src/Navigation.css";
 
-
-function Navigation() {    
-    return (
-        <div className='navBar'>
-            <div className='navBar__leftSide'>
-                <h1>Logo</h1>
-            </div>
-            <div className='navBar__rightSide'>
-                <Link to="/login">Login</Link>
-                <Link to="/signup">Sign Up</Link>
-            </div>
-        </div>
-      );
+function Navigation() {
+  return (
+    <div className="navBar">
+      <div className="navBar__leftSide">
+        <h1>Logo</h1>
+      </div>
+      <div className="navBar__rightSide">
+        <Link to="/login">Login</Link>
+        <Link to="/signup">Sign Up</Link>
+      </div>
+    </div>
+  );
 }
 
 export default Navigation;

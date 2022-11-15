81e13094028584204f10638815ecbda8ad725dbd
Qaisar <syedqaisartanvir@gmail.com>
2022-11-15 17:30:22 +0500

    Update add product form

--- src/Components/AddProduct.jsx
+++ src/Components/AddProduct.jsx
@@ -1,20 +1,71 @@
 import React from "react";
-import {Formik, Form, Field, ErrorMessage} from 'formik'
-import * as Yup from 'yup'
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
 
-function AddProduct() {
-    const initialValues = {
+function ProductForm() {
+  const SUPPORTED_FORMATS = ["image/jpg", "image/jpeg", "image/png"];
+  const initialValues = {
+    title: "",
+    description: "",
+    type: "",
+    price: "",
+    file: [],
+  };
 
-    }
-    return (
+  const validationSchema = Yup.object({
+    title: Yup.string().required("Required Field!"),
+    description: Yup.string().required("Required Field!"),
+    type: Yup.string().required("Required Field!"),
+    price: Yup.number().required("Required Field!"),
+    profileImage: Yup.mixed()
+      .nullable()
+      .required("A file is required")
+      .test(
+        "upload file",
+        (value) => !value || (value && value.size <= 1024 * 1024)
+      )
+      .test(
+        "format",
+        "upload file",
+        (value) => !value || (value && SUPPORTED_FORMATS.includes(value.type))
+      ),
+  });
 
-
-
-
-
-
-
-      );
+  const onSubmit = (values) => console.log("values", values);
+  return (
+    <Formik
+      initialValues={initialValues}
+      validationSchema={validationSchema}
+      onSubmit={onSubmit}
+    >
+      <Form>
+        <div className="form">
+          <div className="form__control">
+            <div className="form__productBasicInfo">
+              <h3>Include Some Details</h3>
+              <div className="form__productAdTitle">
+                <label htmlFor="title">Ad title</label>
+                <Field type="text"></Field>
+                <small>
+                  Mention the key features of your item(e.g. model, brand, type
+                  etc.)
+                </small>
+              </div>
+              <div className="form__productDescription">
+                <label htmlFor="description">Description</label>
+                <Field type="textarea"></Field>
+                <small>include condition, features etc.</small>
+              </div>
+              <div className="form__productType">
+                <label htmlFor="type">Type</label>
+                <Field type="select"></Field>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Form>
+    </Formik>
+  );
 }
 
-export default AddProduct;
+export default ProductForm;

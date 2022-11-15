df33393586e1c48f504ca9bea91f0fec3a29dc17
Qaisar <syedqaisartanvir@gmail.com>
2022-11-15 11:32:59 +0500

    Make a Banner Image component and place a banner image into it

---
+++ src/Components/BannerImage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Banner from "../Images/BannerImage.jpeg";
+
+function BannerImage() {
+  return (
+    <div className="Image">
+      <img src={Banner} alt="Banner Image!"></img>
+    </div>
+  );
+}
+
+export default BannerImage;
